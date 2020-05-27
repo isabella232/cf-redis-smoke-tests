a21e24e01d7d12f5979219062fe360062cd05982
Steve Sienkowski <ssienkowski@pivotal.io>
2020-05-27 17:14:54 -0400

    Smoke-test selected TLS versions with Ruby client.
    * This configures the TLS version smoke tests to use a Redis Ruby client
    with the specified version of TLS, and pass/fail accordingly.
    
    * WIP Note: The value returned in the redis-tls global properties might not
    match the array type we expect (config.json.erb:28):
    `p('global_properties.redis-tls.properties.versions', ['tlsv1',
    'tlsv1.1', 'tlsv1.2'])`
    
    Signed-off-by: Steve Sienkowski <ssienkowski@pivotal.io>

--- service/service_test.go
+++ service/service_test.go
@@ -32,6 +32,27 @@ var _ = Describe("Redis Service", func() {
 		securityGroupName   string
 		serviceKeyName      string
 
+		//  **************************** WIP START 1/3
+		AddSpecTLS = func(app *redis.App, specSteps []*reporter.Step, version string, enabled bool, key string) []*reporter.Step {
+			details := "Reading the value with a TLS" + version + " client should "
+			if enabled {
+				specSteps = append(specSteps, reporter.NewStep(details+"succeed", app.ReadTLSAssert(version, key, "myvalue")))
+			} else {
+				specSteps = append(specSteps, reporter.NewStep(details+"fail", app.ReadTLSAssert(version, key, "protocol not supported:")))
+			}
+			return specSteps
+		}
+
+		HasTLSVersion = func(version string) bool {
+			for _, v := range redisConfig.TLSVersions {
+				if version == v {
+					return true
+				}
+			}
+			return false
+		}
+		//  **************************** WIP END 1/3
+
 		AssertLifeCycleBehavior = func(planName string) {
 			It(strings.ToUpper(planName)+": create, bind to, write to, read from, unbind, and destroy a service instance", func() {
 				var skip bool
@@ -80,21 +101,14 @@ var _ = Describe("Redis Service", func() {
 						app.ReadAssert("mykey", "myvalue"),
 					),
 				}
-				
-				// *********************** WIP START
+
+				//  **************************** WIP START 2/3
 				if redisConfig.TLSEnabled {
-					// read env variable for supported versions of TLS
-					specSteps.push(report.NewStep(
-						"Reading the value with a TLSv1.2 client should succeed ",
-						app.ReadTLSAssert("v1.2", "mykey", "myvalue"),
-					))
-					specSteps.push(report.NewStep(
-						"Reading the value with a TLSv1.1 client should fail ",
-						app.ReadTLSAssert("v1.1", "mykey", "protocol not supported"),
-					))
+					specSteps = AddSpecTLS(app, specSteps, "v1.2", HasTLSVersion("tlsv1.2"), "mykey")
+					specSteps = AddSpecTLS(app, specSteps, "v1.1", HasTLSVersion("tlsv1.1"), "mykey")
+					specSteps = AddSpecTLS(app, specSteps, "v1", HasTLSVersion("tlsv1"), "mykey")
 				}
-				// ** TODO: Add TLSv1/TLSv1.1/TLSv1.2 expects here.
-				// *********************** WIP END
+				//  **************************** WIP END 2/3
 
 				smokeTestReporter.RegisterSpecSteps(specSteps)
 
@@ -258,13 +272,14 @@ var _ = Describe("Redis Service", func() {
 						"Enable tls",
 						testCF.SetEnv(appName, "tls_enabled", "true"),
 					),
-					// *********************** WIP START
+
+					//  **************************** WIP START 3/3
 					reporter.NewStep(
-						"Set TLS Versions to only TLSv1.2",
-						// TODO: Pull versons from configuration.
-						testCF.SetEnv(appName, "tls_versions", ["tlsv1.2"]),
+						"Set TLS Versions to TLSv1.1 and TLSv1.2",
+						// TODO: Verify pulling of versons in config.json.erb
+						testCF.SetEnv(appName, "tls_versions", `["tlsv1.1", "tlsv1.2"]`),
 					),
-					// *********************** WIP END
+					//  **************************** WIP END 3/3
 				}
 
 				smokeTestReporter.ClearSpecSteps()
